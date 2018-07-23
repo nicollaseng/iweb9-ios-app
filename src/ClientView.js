80a0d5a713d6c499331fa8e4790be333dd6c2b2a
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-23 20:21:54 -0300

    client scene completed

--- src/ClientView.js
+++ src/ClientView.js
@@ -27,6 +27,20 @@ export default class iweb9 extends Component {
             </View>
             <Text style={textLogo}> Clientes </Text>
         </View>
+        <View>
+            <Text style={textOne}>Somos uma empresa focada em oferecer o melhor a nossos clientes.
+                Damos a nossos clientes toda a atenção necessária para a elaboração
+                de seus projetos. Desde o desenho à entrega, cuidamos para que nenhum
+                detalhe seja perdido. No final nossos clientes recebem seu aplicativo
+                ou sistema web devidamente testados e com garantia e manutenção periódica.
+            </Text>
+        </View>
+        <View>
+            <Text style={textTwo}>{`Alguns de nosso Clientes: \n`}</Text>
+            <Text style={textOne}>- ATM Consultoria;</Text>
+            <Text style={textOne}>- Erger e Rodney Advocacia;</Text>
+            <Text style={textOne}>- Djas Contabilidade;</Text>
+        </View>
       </View>
     );
   }
@@ -52,16 +66,32 @@ const styles = StyleSheet.create({
   viewTextLogo: {
       justifyContent: 'space-around',
       flexDirection: 'row',
-      alignItems: 'center'
+      alignItems: 'center',
+      marginBottom: 10
   },
   textLogo: {
-      fontSize: 40,
+      fontSize: 50,
       color: '#FF3333',
       fontWeight: 'bold',
       marginLeft: -50,
       marginTop: 30
+  },
+  textOne: {
+      marginTop: 0,
+      marginLeft: 10,
+      textAlign: 'justify',
+      fontSize: 16,
+      color: 'grey'
+  },
+  textTwo: {
+      fontSize: 23,
+      fontWeight: 'bold',
+      color: 'black',
+      marginLeft: 10,
+      marginTop: 10
   }
 });
 
 const {logoStyle, generalView, viewLogoStyle,
-        btnBackStyle, viewTextLogo, textLogo} = styles
+        btnBackStyle, viewTextLogo, textLogo,
+        textOne, textTwo} = styles
