b051a674b87264e539f71b768f92a1c9a478db09
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-23 11:17:10 -0300

    Client navigation implemented

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import {createStackNavigator} from 'react-navigation'
 import MainView from './src/MainView.js'
+import ClientView from './src/ClientView.js'
 import {
   AppRegistry,
   StyleSheet
 } from 'react-native';
 
 const AppNavigator = createStackNavigator({
-  home: MainView
+  home: MainView,
+  client: ClientView
 })
 
 export default class iweb9 extends Component {
--- src/ClientView.js
+++ src/ClientView.js
@@ -7,10 +7,6 @@ import {
 } from 'react-native';
 
 const logo = require('../img/logo_iweb9.png')
-const cliente = require('../img/clientes.png')
-const contato = require('../img/contato.png')
-const servicos = require('../img/servicos.png')
-const empresa = require('../img/empresa.png')
 
 export default class iweb9 extends Component {
     static navigationOptions = {
@@ -21,25 +17,6 @@ export default class iweb9 extends Component {
       <View style={generalView}>
         <View style={viewLogoStyle}>
           <Image source={logo} style={logoStyle}/>
-        </View>
-        <View style={generalViewButtons}>
-            <TouchableOpacity onPress={() => this.props.navigation.navigate('client')}>
-              <Image source={cliente} style={itemsStyle}/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Image source={empresa} style={itemsStyle}/>
-            </TouchableOpacity>
-          </View>
-          <View style={generalViewButtons}>
-            <TouchableOpacity>
-              <Image source={servicos} style={itemsStyle}/>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Image source={contato} style={itemsStyle}/>
-            </TouchableOpacity>
-          </View>
-        <View>
-
         </View>
       </View>
     );
